965c6c40022e51588192e0fc5b3daf8852415555
GeorgeU2030 <georgeu2021@gmail.com>
2024-03-18 15:12:04 -0500

    deploy: config-port-app-deploy

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  await app.listen(3000) || process.env.PORTAPP;
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:4200',
     methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Content-Type, Accept',
   })
-  await app.listen(3000);
 }
 bootstrap();
