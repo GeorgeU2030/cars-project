4c6a599565257f3c62c5befd692ac191e9c22857
GeorgeU2030 <georgeu2021@gmail.com>
2024-03-18 14:33:42 -0500

    feat: cors-configuration-has-been-added

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:4200',
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  })
   await app.listen(3000);
 }
 bootstrap();
